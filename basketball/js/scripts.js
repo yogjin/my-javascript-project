601a79f84aa558748ea76f881f43195c5e7334ce
yogjin <youngjin988@gmail.com>
2021-01-13 11:35:23 +0000

    유저 슛 버튼 동작부분인 onUserShoot() 추가

--- basketball/js/scripts.js
+++ basketball/js/scripts.js
@@ -1,4 +1,5 @@
 let comScore = 0;
+let userScore = 0;
 
 function onComputerShoot(){
 	//슛 타입결정 : 2,3점슛 각각 50%확률로 쏜다 
@@ -28,4 +29,32 @@ function onComputerShoot(){
 		}
 	}
 	comScoreElem.innerHTML = comScore;
+}
+
+function onUserShoot(shootType){
+	let textElem = document.getElementById('text');
+	let userScoreElem = document.getElementById('user-score');
+	if(shootType === 2){//2점슛
+		if(Math.random() < 0.5){
+			//2점슛 50%확률로 성공
+			textElem.innerHTML = '2점슛을 성공했습니다!';
+			userScore += 2;
+		}
+		else{
+			//실패시
+			textElem.innerHTML = '2점슛을 실패했습니다.';
+		}
+	}
+	else{//3점슛
+		if(Math.random() < 0.33){
+			//3점슛 33%확률로 성공
+			textElem.innerHTML = '3점슛을 성공했습니다!';
+			userScore += 3;
+		}
+		else{
+			//실패시
+			textElem.innerHTML = '3점슛을 실패했습니다.';
+		}
+	}
+	userScoreElem.innerHTML = userScore;
 }
