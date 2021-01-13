662363c6667f7b65e93331558d6cbc48c321eb07
yogjin <youngjin988@gmail.com>
2021-01-13 14:25:08 +0000

    refactoring : 중복된 text출력부분 삭제 후 showText() 추가

--- basketball/js/scripts.js
+++ basketball/js/scripts.js
@@ -10,28 +10,27 @@ function onComputerShoot(){
 	}
 	//슛 타입결정 : 2,3점슛 각각 50%확률로 쏜다 
 	let shootType = Math.random() < 0.5 ? 2 : 3;
-	let textElem = document.getElementById('text');
 	let comScoreElem = document.getElementById('computer-score');
 	if(shootType === 2){//2점슛
 		if(Math.random() < 0.5){
 			//2점슛 50%확률로 성공
-			textElem.innerHTML = '컴퓨터가 2점슛을 성공했습니다!';
+			showText('컴퓨터가 2점슛을 성공했습니다!');
 			comScore += 2;
 		}
 		else{
 			//실패시
-			textElem.innerHTML = '컴퓨터가 2점슛을 실패했습니다.';
+			showText('컴퓨터가 2점슛을 실패했습니다.');
 		}
 	}
 	else{//3점슛
 		if(Math.random() < 0.33){
 			//3점슛 33%확률로 성공
-			textElem.innerHTML = '컴퓨터가 3점슛을 성공했습니다!';
+			showText('컴퓨터가 3점슛을 성공했습니다!');
 			comScore += 3;
 		}
 		else{
 			//실패시
-			textElem.innerHTML = '컴퓨터가 3점슛을 실패했습니다.';
+			showText('컴퓨터가 3점슛을 실패했습니다.');
 		}
 	}
 	comScoreElem.innerHTML = comScore;
@@ -54,28 +53,27 @@ function onUserShoot(shootType){
 	if(isComputerTurn){
 		return;
 	}
-	let textElem = document.getElementById('text');
 	let userScoreElem = document.getElementById('user-score');
 	if(shootType === 2){//2점슛
 		if(Math.random() < 0.5){
 			//2점슛 50%확률로 성공
-			textElem.innerHTML = '2점슛을 성공했습니다!';
+			showText('2점슛을 성공했습니다!');
 			userScore += 2;
 		}
 		else{
 			//실패시
-			textElem.innerHTML = '2점슛을 실패했습니다.';
+			showText('2점슛을 실패했습니다.');
 		}
 	}
 	else{//3점슛
 		if(Math.random() < 0.33){
 			//3점슛 33%확률로 성공
-			textElem.innerHTML = '3점슛을 성공했습니다!';
+			showText('3점슛을 성공했습니다!');
 			userScore += 3;
 		}
 		else{
 			//실패시
-			textElem.innerHTML = '3점슛을 실패했습니다.';
+			showText('3점슛을 실패했습니다.');
 		}
 	}
 	userScoreElem.innerHTML = userScore;
@@ -108,13 +106,19 @@ function onUserShoot(shootType){
 		}
 		//승,무,패 각각의 경우
 		if(userScore > comScore){//유저가 이긴경우
-			textElem.innerHTML = '승리했습니다!';
+			showText('승리했습니다!');
 		}
 		else if(userScore === comScore){//비긴경우
-			textElem.innerHTML = '비겼습니다.';
+			showText('비겼습니다.');
 		}
 		else{//유저가 진 경우
-			textElem.innerHTML = '졌습니다..';
+			showText('졌습니다..');
 		}
 	}
+}
+
+//text출력 함수
+function showText(s){
+	let textElem = document.getElementById('text');
+	textElem.innerHTML = s;
 }
