3a7b245563bf32a385a8009912e42f881428f3c2
yogjin <youngjin988@gmail.com>
2021-01-15 15:00:47 +0000

    add : 패스트푸드점 목록 출력

--- fastfood/js/scripts.js
+++ fastfood/js/scripts.js
@@ -10,6 +10,26 @@ $(function(){
 			let total = data.total;
 			
 			$('.total').html('총' + total + '개의 패스트푸드점을 찾았습니다.');
+			
+			let $list = $('.list');
+			
+			for(let i = 0; i < list.length; i++){
+				//각 item 하나하나마다 DOM 객체를 만들어서 $list에 추가한다.
+				let item = list[i];
+				
+				//1. 템플릿을 복제한다.
+				let $elem = $('#item-template')
+					.clone()
+					.removeAttr('id');
+				
+				//2. 복제한 템플릿에 데이터를 세팅한다.
+				$elem.find('.item-no').html(i+1);
+				$elem.find('.item-name').html(item.name);
+				$elem.find('.item-addr').html(item.total);
+				
+				//3. 목록에 복제한 템플릿을 추가한다.
+				$list.append($elem);
+			}
 		});
 	});
 });
