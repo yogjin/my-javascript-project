85339e3331f111f429f78ce8b841cfc4437da2ce
yogjin <youngjin988@gmail.com>
2021-04-26 14:54:40 +0000

    parseId() : url에서 인자(id) 뽑아내는 함수

--- my-trip-advisor/scripts/detail.js
+++ my-trip-advisor/scripts/detail.js
@@ -1,5 +1,15 @@
 //url에서 인자(id) 뽑아내기
 function parseId(str){
-	let s = str.substring(1);
+	let s = str.substring(1);//?id=2 -> id=2
 	
+	let args = s.split('&');
+	
+	for(let i = 0; i < args.length; i++){
+		let arg = args[i];
+		let tokens = arg.split('=');
+		
+		if(tokens[0] == 'id'){
+			return tokens[1];
+		}
+	}
 }
