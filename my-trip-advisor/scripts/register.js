280c9a8e8be0890ecc0a8474b38db7571ec7dcd2
yogjin <youngjin988@gmail.com>
2021-05-03 15:09:47 +0000

    이메일,출생년도 구현

--- my-trip-advisor/scripts/register.js
+++ my-trip-advisor/scripts/register.js
@@ -1,7 +1,12 @@
 $(function(){
+	//출생년도 생성
+	generateYears($('#sel-birth'));
+	let birthSelect = $('#sel-birth').selectmenu();
+	//셀렉트박스 스크롤 설정
+	birthSelect.selectmenu('menuWidget').addClass('overflow');
+	
 	$('form-register').submit(function(e){
 		e.preventDefault();
-		
 		//회원가입 창의 경고문 감추기 -> 필요할 때만 보이기
 		$(this).find('txt-warning').empty().hide();
 		
@@ -18,4 +23,11 @@ $(function(){
 function validateEmail(email) {
 	let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email); // 형식에 맞는 경우 true 리턴	
+}
+
+//출생년도 선택
+function generateYears($select){
+	for(let i = 1970; i <= 2021; i++){
+		$select.append('<option value="'+i+'">'+i+'</option>');
+	}
 }
